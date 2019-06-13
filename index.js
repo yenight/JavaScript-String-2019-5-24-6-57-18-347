0bfa2b9a26d8ff1fe3c6a52cb917ec828e979b35
yenight <30335276+yenight@users.noreply.github.com>
2019-06-13 15:23:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@
 // 完成下面程序，将`name`变量中的字母全部转为为大写，输出：`'HELLO'`。
 var name = 'hello';
 // TODO write your code here...
+console.log(name.toUpperCase())
 
 
 // 完成下面程序，将sentence变量中的所有单词首字母变为大写，输出：'Good Afternoon, Mr Mike.'。
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+
 
 
 // 完成下面程序，将money变量中的金额提取出来，输出：20。
